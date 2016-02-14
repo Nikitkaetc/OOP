89d1127a28d37799db5103d08cc421e4ca983ceb
Kadoshnikov Nikita <nikitka.etc@gmail.com>
2016-02-14 19:54:38 +0300

    added lab1

---
+++ OOPOneLab1/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+bool SearchForWord(const string &strfile, const string &word)
+{
+	if (strfile.find(word) != string::npos)
+	{
+		return true;
+	}
+	return false;
+}
+
+vector<int> FoundWord(const string &filename, const string &word)
+{
+	ifstream f;
+	f.open(filename);
+	if (!f.is_open())
+	{
+		throw invalid_argument("Text not found");
+	}
+	int counterStr = 0;
+	string strfile;
+	vector<int> array1;
+	while (!f.eof())
+	{
+		getline(f, strfile);
+		counterStr++;
+		if (SearchForWord(strfile, word))
+		{
+			array1.push_back(counterStr);
+		}
+	}
+	return array1;
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		cout << "<file name> <text to search>" << endl;
+		return 1;
+	}
+	string filename = argv[1];
+	string word = argv[2];
+	vector<int> array1;
+	try
+	{
+		array1 = FoundWord(filename, word);
+	}
+	catch(const invalid_argument &err)
+	{
+		cout << err.what() << endl;
+	}
+	if (array1.size() == 0)
+	{
+		cout << "Text not found" << endl;
+		return 1;
+	}
+	else
+	{
+		for (const auto &line : array1)
+		{
+			cout << line << endl;
+		}
+	}
+	return 0;
+}
